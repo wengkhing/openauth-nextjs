42794bda8e863fbc4c2cf8f37588dbfb5c009aa1
Weng Khing <wengkhing@gmail.com>
2025-04-22 21:22:35 +0800

    add openauth

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import Image from "next/image";
+import { auth, login, logout } from "./actions";
 import styles from "./page.module.css";
 
-export default function Home() {
+export default async function Home() {
+  const subject = await auth();
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
@@ -14,82 +17,37 @@ export default function Home() {
           priority
         />
         <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
+          {subject ? (
+            <>
+              <li>
+                Logged in as <code>{subject.properties.id}</code>.
+              </li>
+              <li>
+                And then check out <code>app/page.tsx</code>.
+              </li>
+            </>
+          ) : (
+            <>
+              <li>Login with your email and password.</li>
+              <li>
+                And then check out <code>app/page.tsx</code>.
+              </li>
+            </>
+          )}
         </ol>
 
         <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+          {subject ? (
+            <form action={logout}>
+              <button className={styles.secondary}>Logout</button>
+            </form>
+          ) : (
+            <form action={login}>
+              <button className={styles.primary}>Login with OpenAuth</button>
+            </form>
+          )}
         </div>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
